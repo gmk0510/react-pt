7626da808aa08c9ec32b56dd1dd4a54184d1e449
gmk0510 <mingu.ku@gmail.com>
2022-06-09 23:38:27 +0900

    react3

--- react class/blog/src/App.js
+++ react class/blog/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let post = '우동 맛집';
+  let [title, b] = useState('남자 코트 추천')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="blog">
+        <h4>블로그</h4>  
+      </div>
+      <div className="list">
+        <h4>{title}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
