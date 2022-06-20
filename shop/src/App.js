0294dd479b6866f6cd0787e6b136066c8db8e3ca
gmk0510 <mingu.ku@gmail.com>
2022-06-21 01:25:55 +0900

    dw

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar, Container, Nav} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Nav</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">홈</Nav.Link>
+            <Nav.Link href="#features">사진</Nav.Link>
+            <Nav.Link href="#pricing">ㅁㄹ</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
