698cd8c431f41dce3acec2580efc13f0441e04e6
gmk0510 <mingu.ku@gmail.com>
2022-06-30 20:01:04 +0900

    dw

---
+++ nodejs-pt/syntax/number.js
@@ -0,0 +1,12 @@
+console.log(1+1);
+console.log(1*4);
+console.log(1/4);
+console.log(1%4);
+console.log('1+1');
+console.log('1%4');
+console.log('1%4');
+console.log('1%4');
+console.log(Number('2')+3);
+var a = 'aaaaaaaa'
+console.log(a.length);
+
