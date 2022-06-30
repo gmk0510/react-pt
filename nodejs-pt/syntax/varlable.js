cf5b253b885c080cdaef572bc7255327c4645ba4
gmk0510 <mingu.ku@gmail.com>
2022-07-01 00:53:33 +0900

    node pt

---
+++ nodejs-pt/syntax/varlable.js
@@ -0,0 +1,10 @@
+console.log(a);
+var a = 2;
+console.log(a);
+var txt1 = 'ddddddddddddddd ddddd d ddddddddddddd d dddddddddddddddd' +a+ 'ddddddddddd';
+var txt2 = `dd ddddddd ddddddddddddddddd ddddddddddddd${a}`;
+
+
+
+console.log(txt1)
+console.log(txt2)
